fa8eafcbfaa47ee3225858d91a2fe38c75f86134
kr1viah <kravpolet@gmail.com>
2026-01-24 17:34:30 +0100

    Fix buge

--- src/client/java/kr1v/utils/mixin/accessor/ScreenAccessor.java
+++ src/client/java/kr1v/utils/mixin/accessor/ScreenAccessor.java
@@ -3,10 +3,12 @@ package kr1v.utils.mixin.accessor;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(Screen.class)
 public interface ScreenAccessor {
 	@Accessor
+	@Mutable
 	void setClient(MinecraftClient client);
 }
