8984e062c698fda9a0f2aca2327a1bb0ee877823
kr1viah <kravpolet@gmail.com>
2026-08-10 16:27:50 +0200

    first feature :o

--- src/main/java/net/kr1v/devutils/config/Main.java
+++ src/main/java/net/kr1v/devutils/config/Main.java
@@ -1,7 +1,10 @@
 package net.kr1v.devutils.config;
 
+import fi.dy.masa.malilib.hotkeys.KeyAction;
+import fi.dy.masa.malilib.hotkeys.KeybindSettings;
 import kr1v.malilibApi.MalilibApi;
 import kr1v.malilibApi.annotation.Config;
+import kr1v.malilibApi.config.plus.ConfigBooleanHotkeyedPlus;
 import kr1v.malilibApi.config.plus.ConfigHotkeyPlus;
 import net.kr1v.devutils.DevUtils;
 
@@ -11,4 +14,14 @@ public class Main {
 		MalilibApi.openScreenFor(DevUtils.MOD_ID);
 		return true;
 	});
+
+	public static final ConfigBooleanHotkeyedPlus HIDE_SCREENS = new ConfigBooleanHotkeyedPlus("Hide screen rendering", false, "RIGHT_SHIFT")
+			.setAllowExtraKeys(true).setActivateOn(KeyAction.BOTH).setContext(KeybindSettings.Context.ANY);
+	static {
+		HIDE_SCREENS.setPressCallback((action, key) -> {
+			HIDE_SCREENS.set(!HIDE_SCREENS.get());
+			return true;
+		});
+	}
+
 }
---
+++ src/main/java/net/kr1v/devutils/mixin/GuiMixin.java
@@ -0,0 +1,19 @@
+package net.kr1v.devutils.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.kr1v.devutils.config.Main;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.screens.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(Gui.class)
+public class GuiMixin {
+	@WrapOperation(method = "extractRenderState", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;extractRenderStateWithTooltipAndSubtitles(Lnet/minecraft/client/gui/GuiGraphicsExtractor;IIF)V"))
+	private void wrap(Screen instance, GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a, Operation<Void> original) {
+		if (Main.HIDE_SCREENS.getBooleanValue()) return;
+		original.call(instance, graphics, mouseX, mouseY, a);
+	}
+}
