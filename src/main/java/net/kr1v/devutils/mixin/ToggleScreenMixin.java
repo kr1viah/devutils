fb289b055b7dbb4b9d7dcb3506a58fe33be3e101
kr1viah <kravpolet@gmail.com>
2026-08-10 19:32:16 +0200

    make it better

--- src/main/java/net/kr1v/devutils/mixin/ToggleScreenMixin.java
+++ src/main/java/net/kr1v/devutils/mixin/ToggleScreenMixin.java
@@ -3,17 +3,39 @@ package net.kr1v.devutils.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.kr1v.devutils.config.Main;
-import net.minecraft.client.gui.Gui;
+//~ if >=26.1 'Graphics' -> 'GraphicsExtractor'
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(Gui.class)
+@Mixin(
+		//? if >=26.2 {
+		net.minecraft.client.gui.Gui.class
+		//? } else {
+		/*net.minecraft.client.renderer.GameRenderer.class
+		*///? }
+)
 public class ToggleScreenMixin {
-	@WrapOperation(method = "extractRenderState", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;extractRenderStateWithTooltipAndSubtitles(Lnet/minecraft/client/gui/GuiGraphicsExtractor;IIF)V"))
+	//? if >=26.1 {
+	@WrapOperation(method =
+			/*? if >=26.2 {*/
+			"extractRenderState"
+			//? } else if >=26.1 {
+			/*"extractGui"
+			*///?} else {
+			/*"render"
+			*///? }
+			, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;extractRenderStateWithTooltipAndSubtitles(Lnet/minecraft/client/gui/GuiGraphicsExtractor;IIF)V"))
 	private void wrap(Screen instance, GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a, Operation<Void> original) {
 		if (Main.HIDE_SCREENS.getBooleanValue()) return;
 		original.call(instance, graphics, mouseX, mouseY, a);
 	}
+	//? } else {
+	/*@WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;renderWithTooltipAndSubtitles(Lnet/minecraft/client/gui/GuiGraphics;IIF)V"))
+	private void wrap(Screen instance, GuiGraphics guiGraphics, int mouseX, int mouseY, float a, Operation<Void> original) {
+		if (Main.HIDE_SCREENS.getBooleanValue()) return;
+		original.call(instance, guiGraphics, mouseX, mouseY, a);
+	}
+	*///? }
 }
