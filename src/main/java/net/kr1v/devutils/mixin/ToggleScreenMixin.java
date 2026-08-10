0c94764051078af38e8980528e12da17bfc7c421
kr1viah <kravpolet@gmail.com>
2026-08-10 21:11:42 +0200

    1.19.2

--- src/main/java/net/kr1v/devutils/mixin/ToggleScreenMixin.java
+++ src/main/java/net/kr1v/devutils/mixin/ToggleScreenMixin.java
@@ -3,8 +3,11 @@ package net.kr1v.devutils.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.kr1v.devutils.config.Main;
+//? if >=1.20 {
 //~ if >=26.1 'Graphics' -> 'GraphicsExtractor'
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+//? } else
+//import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -31,11 +34,19 @@ public class ToggleScreenMixin {
 		if (Main.HIDE_SCREENS.getBooleanValue()) return;
 		original.call(instance, graphics, mouseX, mouseY, a);
 	}
-	//? } else {
-	/*@WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;renderWithTooltipAndSubtitles(Lnet/minecraft/client/gui/GuiGraphics;IIF)V"))
+	//? } else if >=1.20 {
+	/*//~ if >=1.21.9 'renderWithTooltip' -> 'renderWithTooltipAndSubtitles'
+	@WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;renderWithTooltip(Lnet/minecraft/client/gui/GuiGraphics;IIF)V"))
 	private void wrap(Screen instance, GuiGraphics guiGraphics, int mouseX, int mouseY, float a, Operation<Void> original) {
 		if (Main.HIDE_SCREENS.getBooleanValue()) return;
 		original.call(instance, guiGraphics, mouseX, mouseY, a);
+	}
+	*///? } else {
+	/*//~ if >=1.19.3 'Screen;render' -> 'Screen;renderWithTooltip'
+	@WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;renderWithTooltip(Lcom/mojang/blaze3d/vertex/PoseStack;IIF)V"))
+	private void wrap(Screen instance, PoseStack poseStack, int mouseX, int mouseY, float a, Operation<Void> original) {
+		if (Main.HIDE_SCREENS.getBooleanValue()) return;
+		original.call(instance, poseStack, mouseX, mouseY, a);
 	}
 	*///? }
 }
