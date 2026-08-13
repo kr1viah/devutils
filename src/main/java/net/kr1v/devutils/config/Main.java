1aa239b773a27a5a5ea108e9a6cc0f7c85ddaf57
kr1viah <kravpolet@gmail.com>
2026-08-13 16:22:10 +0200

    wowie

--- src/main/java/net/kr1v/devutils/config/Main.java
+++ src/main/java/net/kr1v/devutils/config/Main.java
@@ -7,7 +7,10 @@ import kr1v.malilibApi.annotation.Config;
 import kr1v.malilibApi.config.plus.ConfigBooleanHotkeyedPlus;
 import kr1v.malilibApi.config.plus.ConfigHotkeyPlus;
 import net.kr1v.devutils.DevUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.MouseHandler;
 
+@SuppressWarnings("unused")
 @Config(DevUtils.MOD_ID)
 public class Main {
 	public static final ConfigHotkeyPlus OPEN_SCREEN = new ConfigHotkeyPlus("Open screen", "R,C").setPressCallback((keyAction, iKeybind) -> {
@@ -17,6 +20,18 @@ public class Main {
 
 	public static final ConfigBooleanHotkeyedPlus HIDE_SCREENS = new ConfigBooleanHotkeyedPlus("Hide screen rendering", false, "RIGHT_SHIFT")
 			.setAllowExtraKeys(true).setActivateOn(KeyAction.BOTH).setContext(KeybindSettings.Context.ANY);
+
+	public static final ConfigHotkeyPlus LOCK_UNLOCK_MOUSE = new ConfigHotkeyPlus("Lock/unlock mouse", "BUTTON_4")
+			.setAllowExtraKeys(true).setPressCallback((action, key) -> {
+				MouseHandler handler = Minecraft.getInstance().mouseHandler;
+				if (handler.isMouseGrabbed()) {
+					handler.releaseMouse();
+				} else {
+					handler.grabMouse();
+				}
+				return true;
+			});
+
 	static {
 		HIDE_SCREENS.setPressCallback((action, key) -> {
 			HIDE_SCREENS.set(!HIDE_SCREENS.get());
